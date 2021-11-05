86c86828b9e8d0824c7c5212ead40228fb6bf727
kevinalexandertello <gaston_alexander@hotmail.es>
2021-11-05 19:06:36 -0300

    adding views

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+// const session = require('express-session');
+// const csrf= require('csurf');
+// const MongoDBStore = require('connect-mongodb-session')(session);
