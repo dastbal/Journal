f78996c9da70e9f40175cc6a68b6d5150ba3ed01
kevinalexandertello <gaston_alexander@hotmail.es>
2021-11-06 19:11:08 -0300

    uploading changes

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const router = require('./routes');
+
+const router = require('./routes')
+
 // const session = require('express-session');
 // const csrf= require('csurf');
 // const MongoDBStore = require('connect-mongodb-session')(session);
 
+const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.set('views','views');
+app.use(bodyParser.urlencoded({extended:false}));
 
+// 
 router(app);
+
+app.listen(port)
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const authRouter = require('./auth.router')
 
-const users
 
 function router(app) {
   const router = express.Router();
   app.use(router);
   router.use('/', authRouter);
-  router.use('/users', usersRouter);
+  //router.use('/users', usersRouter);
 }
 
 module.exports = router;
