c38524ec608d39dfae0c284d2d88559655f31ff1
dastballadares <bal20001@byui.edu>
2021-11-11 19:04:11 -0500

    heroku fixed again

--- libs/mongo.js
+++ libs/mongo.js
@@ -7,8 +7,9 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const port = process.env.PORT || 3000;
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const MONGODB_URL = process.env.MONGODB_URL  || `mongodb+srv://${USER}:${PASSWORD}@cluster0.gimcb.mongodb.net/${config.dbName}?retryWrites=true&w=majority`
-const MONGODB_URI = process.env.MONGODB_URL  || `mongodb+srv://${USER}:${PASSWORD}@cluster0.gimcb.mongodb.net/${config.dbName}`
+const NAME = encodeURIComponent(config.dbName);
+const MONGODB_URL = process.env.MONGODB_URL  || `mongodb+srv://${USER}:${PASSWORD}@cluster0.gimcb.mongodb.net/${NAME}?retryWrites=true&w=majority`
+const MONGODB_URI = process.env.MONGODB_URL  || `mongodb+srv://${USER}:${PASSWORD}@cluster0.gimcb.mongodb.net/${NAME}?`
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true,
