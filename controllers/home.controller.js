ef002bf6f433f20841d69bac29c4f2081a61eaf6
dastballadares <bal20001@byui.edu>
2021-11-15 22:51:17 -0500

    adding icon to the navigation and filter to the journal  and more routes

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -4,3 +4,33 @@ exports.getHome  = (req,res,next)=>{
         pageTitle: 'Home',
     })
 }
+exports.getSpiritual  = (req,res,next)=>{
+    res.render('home/home',{
+        path: '/',
+        pageTitle: 'Spiritual',
+    })
+}
+exports.getNormal  = (req,res,next)=>{
+    res.render('home/home',{
+        path: '/',
+        pageTitle: 'Normal',
+    })
+}
+exports.getJoy  = (req,res,next)=>{
+    res.render('home/home',{
+        path: '/',
+        pageTitle: 'Joy',
+    })
+}
+exports.getLearned  = (req,res,next)=>{
+    res.render('home/home',{
+        path: '/',
+        pageTitle: 'Learned',
+    })
+}
+exports.getSad  = (req,res,next)=>{
+    res.render('home/home',{
+        path: '/',
+        pageTitle: 'Sad',
+    })
+}
