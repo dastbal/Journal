bcbb990db9b0d2db9e53de2c21175c2386b61097
kevinalexandertello <gaston_alexander@hotmail.es>
2021-12-01 00:15:54 -0300

    adding post to edit profile to save user info

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ const authRouter = require('./auth.router')
 const journalRouter = require('./journal.router')
 const registerRouter = require('./register.router')
 const isAuth = require('../middlewares/is-auth')
-const router = express.Router();
-//const homeController = require("../controllers/home");
-//const uploadController = require("../controllers/upload");
 
 function routerJournal(app) {
   const router  = express.Router()
@@ -14,15 +11,4 @@ function routerJournal(app) {
   router.use('/journal', isAuth, journalRouter);
 }
 
-// let routes = app => {
-//   router.get("/", homeController.getHome);
-
-//   router.post("/upload", uploadController.uploadFiles);
-//   router.get("/files", uploadController.getListFiles);
-//   router.get("/files/:name", uploadController.download);
-
-//   return app.use("/", router);
-// };
-
-//module.exports = routes;
 module.exports = routerJournal;
